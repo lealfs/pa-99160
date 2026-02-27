3d7163768f7639ff41159cf6f5c4f733e02444cc
lavivieiraz~ <luisaoliveira643@gmail.com>
2026-02-26 21:41:01 -0300

    funçao correçoes

---
+++ 10. funcao.js
@@ -0,0 +1,15 @@
+// FUNÇÃO
+
+function somar(a,b) {
+    return a + b
+}
+
+function subtrair(a,b) {
+    return a - b
+}
+
+const soma= somar(2,3)
+const subtracao = subtrair(2,3)
+
+console.log('soma: , ${soma}')
+console.log('subtracao: , ${subtracao}');
