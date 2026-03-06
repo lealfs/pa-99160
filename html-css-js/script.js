8e57231ecf29296c30ad60776954ecc172ede392
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 21:35:09 -0300

    site gerar codigo

---
+++ html-css-js/script.js
@@ -0,0 +1,28 @@
+function gerartabuada() {
+    // pegar o valor do input no Html.
+    const numeroinput = document.getElementById("numeroinput");
+    let numero = parseInt(numeroinput);
+
+    // pegar o elemento HTML onde a tabuada será exibida.
+    const resultadoDiv = document.getElementById("resultadotabuada");
+    resultadoDiv.innerHTML = "";
+
+    // verificar se a entrada é um numero válido
+    if (isNaN(numero) || numero === null || numero === "") {
+        resultadoDiv.innerHTML = "Por favor, digite um número...</p>";
+        return;
+    }
+
+    // adiciona um titulo para tabuada.
+    resultadoDiv.innerHTML += `<h2>Tabuada do ${numero}</h2>`;
+
+    // laço de repeticão para gerar a tabuada de 1 até 10.
+    for(let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
+        resultadoDiv.innerHTML += `<p>${numero} x ${i} = ${resultado}</p>`;
+    }
+
+}
+
+const gerarBotao = document.getElementById("gerarBotao");
+gerarBotao.addEventListener("click", gerartabuada);
