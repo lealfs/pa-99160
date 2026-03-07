9725f626f592ec68a7b3f6a70105413b95459bf1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-06 21:16:09 -0300

    correção em js e atividade

--- html-css-js/script.js
+++ html-css-js/script.js
@@ -1,7 +1,7 @@
 function gerartabuada() {
     // pegar o valor do input no Html.
     const numeroinput = document.getElementById("numeroinput");
-    let numero = parseInt(numeroinput);
+    let numero = parseInt(numeroinput.value);
 
     // pegar o elemento HTML onde a tabuada será exibida.
     const resultadoDiv = document.getElementById("resultadotabuada");
@@ -9,12 +9,12 @@ function gerartabuada() {
 
     // verificar se a entrada é um numero válido
     if (isNaN(numero) || numero === null || numero === "") {
-        resultadoDiv.innerHTML = "Por favor, digite um número...</p>";
+        resultadoDiv.innerHTML = "<p>Por favor, digite um número...</p>";
         return;
     }
 
     // adiciona um titulo para tabuada.
-    resultadoDiv.innerHTML += `<h2>Tabuada do ${numero}</h2>`;
+    resultadoDiv.innerHTML += `<h2>Tabuada do número: ${numero}</h2>`;
 
     // laço de repeticão para gerar a tabuada de 1 até 10.
     for(let i = 1; i <= 10; i++) {
