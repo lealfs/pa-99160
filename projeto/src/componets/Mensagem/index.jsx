e365261890f23bbf60a20d9ac2494568f15e86b6
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-26 21:24:20 -0300

    projeto

---
+++ projeto/src/componets/Mensagem/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function Mensagem () {
+    return (
+        <div className='Mensagem'>
+            <h1>componente</h1>
+            <p>Este é um componente simples.</p>
+        </div>
+    )
+}
+
+// export defaulth Mensagem
